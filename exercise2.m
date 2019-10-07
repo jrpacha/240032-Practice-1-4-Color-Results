31e0cf157ead72faefc4b858c92d7b128d42934f
Juan R. Pacha <juan.ramon.pacha@upc.edu>
2019-10-07 18:59:32 +0200

    Added one of the files feft in the last commit

---
+++ exercise2.m
@@ -0,0 +1,18 @@
+clearvars
+close all
+
+%Exercise2
+%Use the function plotContourSolution to draw the interpolated color map
+%in a quadrengular mesh
+
+eval('meshTwoHolesQuad');
+numNodes=size(nodes,1);
+numElem=size(elem,1);
+
+temp=1:numNodes;
+
+colorMap='jet'
+title='Temperature Plot'
+
+plotContourSolution(nodes,elem,temp,title,colorMap); %Can plot both triang.
+                                                     %and quad. meshes!
